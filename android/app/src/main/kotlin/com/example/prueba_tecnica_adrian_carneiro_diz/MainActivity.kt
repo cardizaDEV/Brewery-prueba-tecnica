166ab1ad9052e1615ab92bed341b14406b444b34
cardizaDEV <cardizaDEV@gmail.com>
2023-03-16 18:19:31 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/example/prueba_tecnica_adrian_carneiro_diz/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.prueba_tecnica_adrian_carneiro_diz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/brew_cell.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BrewCell extends StatelessWidget {
+  const BrewCell({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
